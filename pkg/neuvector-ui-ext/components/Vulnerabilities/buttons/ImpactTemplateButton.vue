d27c85faa68aae359cbd22af3319035442d2c0e1
Rush Kapoor <rush.kapoor@berkeley.edu>
2024-05-20 19:55:45 -0400

    Vulnerabilities

---
+++ pkg/neuvector-ui-ext/components/Vulnerabilities/buttons/ImpactTemplateButton.vue
@@ -0,0 +1,12 @@
+<script>
+    export default {
+        props: {
+            content: Object,
+            type: String
+        }
+    }
+</script>
+
+<template>
+    <p>{{ content }}</p>
+</template>
